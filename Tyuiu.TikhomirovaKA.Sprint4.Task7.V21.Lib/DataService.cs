82ef856146d94d018a6cd462480cd9b8dfdd91ac
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-11-10 14:41:10 +0500

    Task7

--- Tyuiu.TikhomirovaKA.Sprint4.Task7.V21.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint4.Task7.V21.Lib/DataService.cs
@@ -1,7 +1,22 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint4.Task7.V21.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
 
+namespace Tyuiu.TikhomirovaKA.Sprint4.Task7.V21.Lib
+{
+    public class DataService : ISprint4Task7V21
+    {
+        public int Calculate(int n, int m, string value)
+        {
+            int s = 0;
+            int[,] matrix = new int[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = int.Parse(value[i*m+j].ToString());
+                    if (matrix[i, j] % 2 == 0) s += matrix[i, j];
+                }
+            }
+            return s;
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint4.Task7.V21/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint4.Task7.V21/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint4.Task7.V21.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #4                                                              *");
+        Console.WriteLine("* Тема: Обработка структурных типов                                      *");
+        Console.WriteLine("* Задание #7                                                             *");
+        Console.WriteLine("* Вариант #21                                                            *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        string str = "425963128528"; int n = 4; int m = 3;
+        Console.WriteLine(str);
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+        int res = ds.Calculate(n, m, str);
+        int[,] matrix = new int[n, m];
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                matrix[i, j] = int.Parse(str[i * m + j].ToString());
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("Сумма чётных чисел массива: " + res);
+        Console.ReadKey();
+    }
+}
