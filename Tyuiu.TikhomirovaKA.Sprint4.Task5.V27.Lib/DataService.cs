c0c2a274ba7e1c55eed5092708933dd61e505581
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-11-10 13:53:09 +0500

    Task5

--- Tyuiu.TikhomirovaKA.Sprint4.Task5.V27.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint4.Task5.V27.Lib/DataService.cs
@@ -1,7 +1,22 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint4.Task5.V27.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
 
+namespace Tyuiu.TikhomirovaKA.Sprint4.Task5.V27.Lib
+{
+    public class DataService : ISprint4Task5V27
+    {
+        
+        public int Calculate(int[,] matrix)
+        {
+            int count = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] < 0) count++;
+                }
+            }
+            return count;
+        }
+        
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint4.Task5.V27/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint4.Task5.V27/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+using Tyuiu.TikhomirovaKA.Sprint4.Task5.V27.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #4                                                              *");
+        Console.WriteLine("* Тема: Обработка структурных типов                                      *");
+        Console.WriteLine("* Задание #5                                                             *");
+        Console.WriteLine("* Вариант #27                                                            *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        int[,] array = new int[5, 5];
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = RandomNumberGenerator.GetInt32(-5, 7);
+            }
+        }
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine(ds.Calculate(array));
+        Console.ReadKey();
+    }
+}
