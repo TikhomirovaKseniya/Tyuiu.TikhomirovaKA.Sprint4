9e7c235c7c8110d56a105b69819020e70117250b
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-11-10 14:19:14 +0500

    Task6

--- Tyuiu.TikhomirovaKA.Sprint4.Task6.V18.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint4.Task6.V18.Lib/DataService.cs
@@ -1,7 +1,23 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint4.Task6.V18.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
 
+namespace Tyuiu.TikhomirovaKA.Sprint4.Task6.V18.Lib
+{
+    public class DataService : ISprint4Task6V18
+    {
+        public string[] Calculate(string[] array)
+        {     
+            int count = array.Count(a => a.Length > 6);
+            string[] result = new string[count];
+            int i = 0;       
+            foreach (string word in array)
+            {   
+                if (word.Length > 6)
+                {                       
+                    result[i] = word;
+                    i++;
+                }
+            }
+            return result;
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint4.Task6.V18/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint4.Task6.V18/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint4.Task6.V18.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #4                                                              *");
+        Console.WriteLine("* Тема: Обработка структурных типов                                      *");
+        Console.WriteLine("* Задание #6                                                             *");
+        Console.WriteLine("* Вариант #18                                                            *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        string[] array = { "Собака", "Кошка", "Кролик", "Хомяк", "Попугай", "Рыбка", "Черепаха" };
+        Console.WriteLine(string.Join(", ", array));
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine(string.Join(", ", ds.Calculate(array)));
+        Console.ReadKey();
+    }
+}
