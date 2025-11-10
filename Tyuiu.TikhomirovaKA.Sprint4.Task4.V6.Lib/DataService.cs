df794ae820e546946ad6aff66d6a75513d303fac
Ксения Тихомирова <tuxuu.mup@gmail.com>
2025-11-10 13:46:00 +0500

    Task4

--- Tyuiu.TikhomirovaKA.Sprint4.Task4.V6.Lib/DataService.cs
+++ Tyuiu.TikhomirovaKA.Sprint4.Task4.V6.Lib/DataService.cs
@@ -1,7 +1,19 @@
-﻿namespace Tyuiu.TikhomirovaKA.Sprint4.Task4.V6.Lib
-{
-    public class DataService
-    {
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
 
+namespace Tyuiu.TikhomirovaKA.Sprint4.Task4.V6.Lib
+{
+    public class DataService : ISprint4Task4V6
+    {
+        public int[,] Calculate(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] % 2 != 0) matrix[i, j] = 0;
+                }
+            }
+            return matrix;
+        }
     }
 }
--- Tyuiu.TikhomirovaKA.Sprint4.Task4.V6/Program.cs
+++ Tyuiu.TikhomirovaKA.Sprint4.Task4.V6/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Tyuiu.TikhomirovaKA.Sprint4.Task4.V6.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* Спринт #4                                                              *");
+        Console.WriteLine("* Тема: Обработка структурных типов                                      *");
+        Console.WriteLine("* Задание #4                                                             *");
+        Console.WriteLine("* Вариант #6                                                            *");
+        Console.WriteLine("* Выполнила: Выполнила Тихомирова К. А. | ИБКСб-25-1                     *");
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+        Console.WriteLine("**************************************************************************");
+        int[,] array = new int[,]
+       {
+            { 7, 6, 6, 6, 9 },
+            { 4, 4, 4, 9, 4 },
+            { 5, 5, 5, 6, 5 },
+            { 4, 9, 5, 9, 7 },
+            { 6, 9, 5, 6, 7 }
+       };
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + ", ");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("**************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+        Console.WriteLine("**************************************************************************");
+        int[,] matrix = ds.Calculate(array);
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + ", ");
+            }
+            Console.WriteLine();
+        }
+        Console.ReadKey();
+    }
+}
